cdcc4307c9b6ca01d7a8d6a49697a35dd1c3aa66
Jeff Allen <jallen3095@gmail.com>
2018-10-18 20:08:39 -0500

    Initial Spring files

---
+++ src/main/java/omahelp/controllers/EventController.java
@@ -0,0 +1,29 @@
+package omahelp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EventController {
+
+    @GetMapping("/event/list")
+    public String list(TODO)
+
+    @GetMapping("/event/search")
+    public String search(TODO)
+
+    // TODO - {id} GET
+    @GetMapping()
+
+    // TODO - {id} DELETE
+    @GetMapping()
+
+    @GetMapping("/event/register")
+    public String register(TODO)
+
+    // TODO - edit/{id}
+    @GetMapping()
+    
+}
---
+++ src/main/java/omahelp/controllers/UserController.java
@@ -0,0 +1,17 @@
+package omahelp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user/register")
+    public String register(TODO)
+
+    @GetMapping("/user/login")
+    public String login(TODO)
+    
+}
