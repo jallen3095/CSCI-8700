83e1985e904294096cf9af1069a98872796366d5
Andrew Hassing <hassiar@huskers.unl.edu>
2018-10-28 21:19:28 -0500

    adding some event stuff

--- omahelp-rest/src/main/java/edu/uno/omahelp/event/EventController.java
+++ omahelp-rest/src/main/java/edu/uno/omahelp/event/EventController.java
@@ -1,11 +1,21 @@
 package edu.uno.omahelp.event;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
+@RequestMapping("event/")
 public class EventController {
 
+  @Autowired
+    private EventDao eventDao;
     //@RequestMapping("/event/list")
     //public List<Event> listEvents() {}
 
--- omahelp-rest/src/main/java/edu/uno/omahelp/event/EventDao.java
+++ omahelp-rest/src/main/java/edu/uno/omahelp/event/EventDao.java
@@ -12,7 +12,44 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import edu.uno.omahelp.user.User;
-
 @Component
-class EventDao {}
+class EventDao {
+
+      public List<Event> listAllEvents() throws URISyntaxException, SQLException {
+          Connection connection = getConnection();
+          Statement stmt = connection.createStatement();
+          String sql;
+          sql = "SELECT * FROM \"Event\"";
+          ResultSet rs = stmt.executeQuery(sql);
+          List<Organization> orgs = new ArrayList<>();
+          while(rs.next()) {
+            Event event = new Event();
+              event.setId(rs.getInt("event_id"));
+              event.setName(rs.getString("event_name"));
+              event.setLocation(rs.getString("event_address"));
+              event.setDate(rs.getString("event_date"));
+              event.setDescription(rs.getString("event_description"));
+              orgs.add(org);
+          }
+
+          return orgs;
+      }
+
+      private Connection getConnection() throws URISyntaxException, SQLException {
+          URI dbURI = null;
+
+          if(System.getenv("DATABASE_URL") != null) {
+              dbURI = new URI(System.getenv("DATABASE_URL"));
+          } else {
+              String DATABASE_URL = "postgres://wajuhzuzopvjva:15e166e3b55bacf46b2e4040ddd127fa17ac8b5f6da3fd602961415e8ff6273f@ec2-54-225-76-201.compute-1.amazonaws.com:5432/db0fu95pd1fvq";
+              dbURI = new URI(DATABASE_URL);
+          }
+
+      String username = dbURI.getUserInfo().split(":")[0];
+      String password = dbURI.getUserInfo().split(":")[1];
+      String dbUrl = "jdbc:postgresql://" + dbURI.getHost() + ':'
+                  + dbURI.getPort() + dbURI.getPath()
+                  + "?sslmode=require";
+      return DriverManager.getConnection(dbUrl, username, password);
+    }
+}
