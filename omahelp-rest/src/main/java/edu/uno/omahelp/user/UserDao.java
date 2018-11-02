bb73b9dba2b6b3f59be0b90f5796e6edc40426bc
jallen3095@gmail.com <jallen3095@gmail.com>
2018-11-01 19:58:14 -0500

    Remove unneeded import

--- omahelp-rest/src/main/java/edu/uno/omahelp/user/UserDao.java
+++ omahelp-rest/src/main/java/edu/uno/omahelp/user/UserDao.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This class contains the methods necessary to handle the User database operations
